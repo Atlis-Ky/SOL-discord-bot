e92d4c3b37ded3e3902c7dd452f2af312489a0c7
Ky <kylesmith97@outlook.com>
2024-06-23 21:31:37 +0100

    SOL bot by Atlis-Ky :
    SOL bot by Atlis-Ky :) full app commit

---
+++ main.js
@@ -0,0 +1,53 @@
+import Discord from "discord.js";
+import dotenv from "dotenv";
+import ms from "ms";
+
+dotenv.config();
+
+const log = console.log;
+const error = console.error;
+
+const client = new Discord.Client({
+  shards: "auto",
+  intents: [Discord.GatewayIntentBits.Guilds],
+});
+
+log(process.env.TOKEN ? "✅ Found TOKEN Env var" : "❌ Missing TOKEN Env var");
+
+async function login() {
+  log("Attempting login...");
+  try {
+    await client.login(process.env.TOKEN);
+    
+    log("🤖 Logged in");
+  } catch (e) {
+    log("🫤 Failed to login, error:", e);
+  }
+}
+
+async function getPrice() {
+  try {
+    const raw = await fetch("https://api.coinbase.com/v2/prices/sol-usd/spot");
+    const { data } = await raw.json();
+    return Math.round(+data.amount * 100) / 100;
+  } catch (e) {
+    error("Failed to fetch from coinbase api, error:", e);
+  }
+}
+console.log(await getPrice());
+
+async function setBotActivity() {
+  const price = await getPrice();
+  if (!price) return;
+
+  const ClientPresence = await client.user.setActivity(`$${price}`, {
+    type: Discord.ActivityType.Watching,
+  });
+
+  log(`Activity set to ${ClientPresence.activities[0].name}`);
+}
+
+(async function main() {
+  await login();
+  setInterval(setBotActivity, ms("30 sec"));
+})();
